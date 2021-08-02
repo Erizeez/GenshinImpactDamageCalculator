1178dee2fa644c77c4e19dd04e4ba38f3e6b11ce
erizeez <820540373@qq.com>
2021-08-02 16:28:48 +0800

    Init the server part.

---
+++ GenshinImpactDamageCalculatorServer/src/main/java/com/erizeez/genshinimpactdamagecalculatorserver/GenshinImpactDamageCalculatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.erizeez.genshinimpactdamagecalculatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinImpactDamageCalculatorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinImpactDamageCalculatorServerApplication.class, args);
+    }
+
+}
---
+++ GenshinImpactDamageCalculatorServer/src/test/java/com/erizeez/genshinimpactdamagecalculatorserver/GenshinImpactDamageCalculatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erizeez.genshinimpactdamagecalculatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinImpactDamageCalculatorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
