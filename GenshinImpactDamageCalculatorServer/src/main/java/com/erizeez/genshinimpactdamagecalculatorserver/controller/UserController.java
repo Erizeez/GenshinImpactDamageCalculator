bd1de831c9b365de166b1ad724d8e3e3f8e9b639
Rakutenipoi <43430994+Rakutenipoi@users.noreply.github.com>
2021-08-07 20:08:16 +0800

    添加用户登录

--- GenshinImpactDamageCalculatorServer/src/main/java/com/erizeez/genshinimpactdamagecalculatorserver/controller/UserController.java
+++ GenshinImpactDamageCalculatorServer/src/main/java/com/erizeez/genshinimpactdamagecalculatorserver/controller/UserController.java
@@ -60,10 +60,31 @@ public class UserController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String, Object> login(@RequestBody User login_form) {
-        Map<String, Object> response = new HashMap<>();
+    public Map<String, Object> login(@RequestBody User login_form, HttpServletResponse response) throws IOException {
+        Map<String, Object> map = new HashMap<>();
 
+        if (login_form.getUserName() != null && login_form.getPassWord() != null) {
+            if (userService.selectUserByUserName(login_form.getUserName()) == null){
+                map.put("msg", "There is no user named " + login_form.getUserName());
+                map.put("result", "-1");
+            } else if (userService.selectUserByUserName(login_form.getUserName()).getPassWord() != login_form.getPassWord()){
+                map.put("msg", "Password error.");
+                map.put("result", "-1");
+            } else {
+                User user = userService.selectUserByUserName(login_form.getUserName());
 
-        return response;
+                Cookie cookie = new Cookie("token", TokenUtil.makeToken(user));
+                cookie.setPath("/");
+                cookie.setMaxAge(TokenUtil.EXPIRE_TIME_MIN * 60);
+                response.addCookie(cookie);
+                map.put("msg", "Login process is complete.");
+                map.put("result", "0");
+            }
+        } else {
+            map.put("msg", "Login form is incomplete.");
+            map.put("result", "-1");
+        }
+
+        return map;
     }
 }
